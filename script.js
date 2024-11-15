b581c485abade1a59db47b949fef290c70c7e91a
Rohit Kumar <patelrr2552001@gmail.com>
2024-11-15 14:46:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+let freqObj = {}
+
+for (let i = 0; i < str.length; i++) {
+    if (!freqObj[str.charAt(i)]) {
+        freqObj[str.charAt(i)] = 1
+    } else {
+        freqObj[str.charAt(i)] = freqObj[str.charAt(i)] + 1
+    }
+}
+
+for (let item in freqObj) {
+    if (freqObj[item] == 1) {
+        return item;
+    } else {
+        return null
+    }
+}
+console.log(freqObj);
+	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
